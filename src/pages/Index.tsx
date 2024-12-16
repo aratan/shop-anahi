3cc7ee12724765b767df3df4ed25bf2b77ef7392
Victor Arbiol <victor.arbiol@gmail.com>
2024-12-16 13:28:10 +0100

    logo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -97,7 +97,12 @@ const Index = () => {
               <Button variant="ghost" size="icon">
                 <Menu className="h-6 w-6" />
               </Button>
-              <img src="https://lasalhajasdetoledoymas.com/cdn/shop/files/Screenshot_20231214_090808_ad92fa08-082b-4d36-9beb-876727ffa229.jpg?v=1704297589&width=150"></img>
+              <img 
+              src="https://lasalhajasdetoledoymas.com/cdn/shop/files/Screenshot_20231214_090808_ad92fa08-082b-4d36-9beb-876727ffa229.jpg?v=1704297589&width=150" 
+              style="width: 100px; height: auto;" 
+              alt="Logo"
+              />
+
               <Link to="/" className="ml-4 text-2xl font-serif">LUXURY SCENTS</Link>
             </div>
             <div className="hidden md:flex space-x-8">
